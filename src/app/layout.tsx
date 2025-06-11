4abebb82d40c63e5408f202a1229ddb413cb6ab0
໓ēēงคຖk <deevank15673@gmail.com>
2025-06-11 14:55:05 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased min-h-screen flex flex-col bg-background">
+      <body className="font-body antialiased min-h-screen flex flex-col bg-background" suppressHydrationWarning={true}>
         <AppInitializer>
           {/* <SiteHeader /> */} {/* SiteHeader component removed */}
           <main className="flex-grow container mx-auto p-4 md:p-6">
