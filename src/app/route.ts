056eca37629247c3b349a685edade199b5c70e33
StarDeevank <deevank15673@gmail.com>
2025-06-12 18:56:38 +0530

    Delete src/app/route.ts

--- src/app/route.ts
+++
@@ -1,6 +0,0 @@
-export async function GET() {
-  return new Response(
-    JSON.stringify({ hasKey: !!process.env.GOOGLE_API_KEY }),
-    { headers: { "Content-Type": "application/json" } }
-  );
-}
